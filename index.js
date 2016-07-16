90b7d14bb51b448b7068c6f6b84693fe365012ed
Mike Fotinakis <mike@fotinakis.com>
2016-07-15 20:41:33 -0700

    temp disable fastboot check

--- index.js
+++ index.js
@@ -114,10 +114,10 @@ module.exports = {
   // After build output is ready, create a Percy build and upload missing build resources.
   outputReady: function(result) {
     // Disable Percy when running Fastboot builds (for now).
-    if (typeof process.env.EMBER_CLI_FASTBOOT !== 'undefined') {
-      console.warn('[percy] Disabling Percy in Fastboot build.')
-      return;
-    }
+    // if (typeof process.env.EMBER_CLI_FASTBOOT !== 'undefined') {
+    //   console.warn('[percy] Disabling Percy in Fastboot build.')
+    //   return;
+    // }
 
     var token = process.env.PERCY_TOKEN;
     var apiUrl = process.env.PERCY_API; // Optional.
