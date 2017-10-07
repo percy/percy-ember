adcd1ffe2d17ec1de44b92be0eeff50263039763
Jeff Jewiss <jeffjewiss@gmail.com>
2017-10-07 14:46:21 +0100

    use babel polyfill in testing

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,11 @@
-/*jshint node:true*/
 /* global require, module */
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    // Add options here
+    'ember-cli-babel': {
+      includePolyfill: (EmberAddon.env() === 'test'),
+    }
   });
 
   /*
