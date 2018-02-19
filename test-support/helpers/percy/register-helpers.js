f31cbf18e67b3335c7f4217f3197332951f41c2a
Mike Fotinakis <mike@fotinakis.com>
2018-02-19 15:36:08 -0800

    After ember init.

--- test-support/helpers/percy/register-helpers.js
+++ test-support/helpers/percy/register-helpers.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import { percySnapshot } from 'ember-percy';
+import { registerAsyncHelper } from '@ember/test';
 
 Ember.Test.registerAsyncHelper('percySnapshot', function(app, name, options) {
   percySnapshot(name, options);
