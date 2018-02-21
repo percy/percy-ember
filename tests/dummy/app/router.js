8f1c206425d8a40db0ab3df7e075e093bbc78b95
Mike Fotinakis <mike@fotinakis.com>
2018-02-20 20:26:34 -0800

    Fix styles/attributes tied to ember-application container.

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,12 +1,19 @@
+import $ from 'jquery';
+import { on } from '@ember/object/evented';
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+
+  addDataRoute: on('didTransition', function() {
+    $('.ember-application').attr('data-route', this.currentRouteName);
+  }),
 });
 
 Router.map(function() {
+  this.route('test-route-styles');
 });
 
 export default Router;
