28bee3d0a4504d322a4e37477a42dbbb49d38c53
LC <lrc3233@gmail.com>
2018-04-09 14:45:34 -0700

    ignore old import syntax lint error

--- test-support/helpers/percy/register-helpers.js
+++ test-support/helpers/percy/register-helpers.js
@@ -1,7 +1,7 @@
 import { percySnapshot } from 'ember-percy';
 import Ember from 'ember';
 
-Ember.Test.registerAsyncHelper('percySnapshot', function(app, name, options) {
+Ember.Test.registerAsyncHelper('percySnapshot', function(app, name, options) { // eslint-disable-line
   percySnapshot(name, options);
 });
 
