40cb769830e5688238c34ffa7b9f03b2d9e53d15
Robert DeLuca <robertdeluca19@gmail.com>
2020-09-21 12:28:55 -0500

    v2.1.4

--- addon-test-support/@percy/ember/index.js
+++ addon-test-support/@percy/ember/index.js
@@ -30,7 +30,7 @@ function envInfo() {
 }
 
 function clientInfo() {
-  return `@percy/ember@v2.1.3`;
+  return `@percy/ember@v2.1.4`;
 }
 
 // This will only remove the transform applied by Ember's defaults
