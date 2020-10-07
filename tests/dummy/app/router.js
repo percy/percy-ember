23c6aaae77ca70ce91d01f9572b941d8651e5b04
Wil Wilsman <wil@wilwilsman.com>
2020-10-07 15:06:58 -0500

    🏗 Freshly recreate addon architecture

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'dummy/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
@@ -7,5 +7,4 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  this.route('test-route-styles');
 });
