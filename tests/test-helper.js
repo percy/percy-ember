7d68c80cb1da671f95230cf61fa6d0bd0612c0ee
Wil Wilsman <wil@wilwilsman.com>
2020-10-12 15:34:53 -0500

    ✅ Rewrite tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,14 @@
+import QUnit from 'qunit';
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
-setApplication(Application.create(config.APP));
+QUnit.assert.matches = function matches(actual, regex, message) {
+  var result = !!regex && !!actual && (new RegExp(regex)).test(actual);
+  var expected = `String matching ${regex.toString()}`;
+  this.pushResult({ result, actual, expected, message });
+};
 
-start();
+setApplication(Application.create(config.APP));
+start({ setupEmberOnerrorValidation: false });
