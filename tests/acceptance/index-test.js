03fe3e3a461b2d2ecacd099c1230d287ade2dc8e
Wil Wilsman <wil@wilwilsman.com>
2022-08-15 13:27:26 -0500

    ✅ Use Percy CLI testing mode for tests (#628)
    Updates tests to utilize CLI's new testing mode and updated `@percy/sdk-utils` test helpers.

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,50 +1,36 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import helpers from '@percy/sdk-utils/test/client';
+import helpers from '@percy/sdk-utils/test/helpers';
 import percySnapshot from '@percy/ember';
 
 module('percySnapshot', hooks => {
   setupApplicationTest(hooks);
 
   hooks.beforeEach(async () => {
-    await helpers.setup();
+    await helpers.setupTest();
     // mock mocha env info
     window.mocha = { version: '1.2.3' };
   });
 
-  hooks.afterEach(async () => {
-    await helpers.teardown();
-  });
-
   test('disables snapshots when the healthcheck fails', async assert => {
-    await helpers.testFailure('/percy/healthcheck');
+    await helpers.test('error', '/percy/healthcheck');
 
     await percySnapshot('Snapshot 1');
     await percySnapshot('Snapshot 2');
 
-    assert.deepEqual(await helpers.getRequests(), [
-      ['/percy/healthcheck']
-    ]);
-
-    assert.deepEqual(helpers.logger.stderr, []);
-    assert.deepEqual(helpers.logger.stdout, [
-      '[percy] Percy is not running, disabling snapshots'
+    assert.contains(await helpers.get('logs'), [
+      'Percy is not running, disabling snapshots'
     ]);
   });
 
   test('disables snapshots when the healthcheck encounters an error', async assert => {
-    await helpers.testError('/percy/healthcheck');
+    await helpers.test('disconnect', '/percy/healthcheck');
 
     await percySnapshot('Snapshot 1');
     await percySnapshot('Snapshot 2');
 
-    assert.deepEqual(await helpers.getRequests(), [
-      ['/percy/healthcheck']
-    ]);
-
-    assert.deepEqual(helpers.logger.stderr, []);
-    assert.deepEqual(helpers.logger.stdout, [
-      '[percy] Percy is not running, disabling snapshots'
+    assert.contains(await helpers.get('logs'), [
+      'Percy is not running, disabling snapshots'
     ]);
   });
 
@@ -52,33 +38,33 @@ module('percySnapshot', hooks => {
     await percySnapshot('Snapshot 1');
     await percySnapshot('Snapshot 2');
 
-    let reqs = await helpers.getRequests();
+    let reqs = await helpers.get('requests');
 
-    assert.equal(reqs[0][0], '/percy/healthcheck');
-    assert.equal(reqs[1][0], '/percy/dom.js');
-    assert.equal(reqs[2][0], '/percy/snapshot');
-    assert.equal(reqs[3][0], '/percy/snapshot');
+    assert.equal(reqs[0].url, '/percy/healthcheck');
+    assert.equal(reqs[1].url, '/percy/dom.js');
+    assert.equal(reqs[2].url, '/percy/snapshot');
+    assert.equal(reqs[3].url, '/percy/snapshot');
 
-    assert.equal(reqs[2][1].name, 'Snapshot 1');
-    assert.matches(reqs[2][1].url, /^http:\/\/localhost:7357/);
-    assert.matches(reqs[2][1].domSnapshot, /<body class="ember-application"><\/body>/);
-    assert.matches(reqs[2][1].clientInfo, /@percy\/ember\/\d.+/);
-    assert.matches(reqs[2][1].environmentInfo[0], /ember\/.+/);
-    assert.matches(reqs[2][1].environmentInfo[1], /qunit\/.+/);
+    assert.equal(reqs[2].body.name, 'Snapshot 1');
+    assert.matches(reqs[2].body.url, /^http:\/\/localhost:7357/);
+    assert.matches(reqs[2].body.domSnapshot, /<body class="ember-application"><\/body>/);
+    assert.matches(reqs[2].body.clientInfo, /@percy\/ember\/\d.+/);
+    assert.matches(reqs[2].body.environmentInfo[0], /ember\/.+/);
+    assert.matches(reqs[2].body.environmentInfo[1], /qunit\/.+/);
 
-    assert.equal(reqs[3][1].name, 'Snapshot 2');
+    assert.equal(reqs[3].body.name, 'Snapshot 2');
   });
 
   test('generates a snapshot name from qunit assert', async assert => {
     await percySnapshot(assert);
-    assert.equal((await helpers.getRequests())[1][1].name, (
+    assert.equal((await helpers.get('requests'))[1].body.name, (
       'percySnapshot | generates a snapshot name from qunit assert'));
   });
 
   test('generates a snapshot name from mocha\'s test', async assert => {
     // mocked since this is not a mocha test
     await percySnapshot({ fullTitle: () => 'Mocha | generated name' });
-    assert.equal((await helpers.getRequests())[1][1].name, 'Mocha | generated name');
+    assert.equal((await helpers.get('requests'))[1].body.name, 'Mocha | generated name');
   });
 
   test('copies scoped attributes to the body element', async assert => {
@@ -88,17 +74,17 @@ module('percySnapshot', hooks => {
 
     await percySnapshot('Snapshot 1');
 
-    assert.matches((await helpers.getRequests())[1][1].domSnapshot, (
+    assert.matches((await helpers.get('requests'))[1].body.domSnapshot, (
       /<body class="ember-application custom-classname" data-test="true"><\/body>/));
   });
 
   test('handles snapshot errors', async assert => {
-    await helpers.testFailure('/percy/snapshot', 'testing');
+    await helpers.test('error', '/percy/snapshot');
 
     await percySnapshot('Snapshot 1');
 
-    assert.deepEqual(helpers.logger.stdout, []);
-    assert.deepEqual(helpers.logger.stderr[0], '[percy] Could not take DOM snapshot "Snapshot 1"');
-    assert.matches(helpers.logger.stderr[1], /^\[percy] Error: testing/);
+    assert.contains(await helpers.get('logs'), [
+      'Could not take DOM snapshot "Snapshot 1"'
+    ]);
   });
 });
