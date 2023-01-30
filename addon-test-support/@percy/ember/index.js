1f5de366af57316bf31552aa6be39b8fbbf29fe4
Jigar wala <jigarwala007@gmail.com>
2023-01-30 19:30:16 +0530

    :bug: fix shadow DOM serialization (#706)

--- addon-test-support/@percy/ember/index.js
+++ addon-test-support/@percy/ember/index.js
@@ -34,7 +34,7 @@ function scopeDOM(scope, dom) {
   if (!$scoped) return;
 
   // replace body content with scoped content
-  $body.innerHTML = $scoped.innerHTML;
+  $body.replaceChildren(...$scoped.children);
 
   // copy scoped attributes to the body element
   for (let i = 0; i < $scoped.attributes.length; i++) {
